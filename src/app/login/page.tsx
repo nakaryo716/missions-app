21c68263e8bfae6ec47a83fc8b5729fb2d021cf7
ryonakaya <nakayaryo0716@gmail.com>
2025-01-11 17:00:42 +0900

    [add]Added Login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,34 @@
-export default function Login() {
+'use client'
+import Login from "@/components/Login";
+import { Box } from "@mui/material";
+import { ChangeEvent, useState } from "react";
+
+export default function SignUpPage() {
+
+  const handleSubmit = () => {
+    alert(email + password);
+  };
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSetEmail = (e:  ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSetPassword = (e:  ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setPassword(e.target.value);
+  };
+
   return(
     <>
-      <h1>Login page</h1>
+      <Box sx={{display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh"}}>
+        <Login
+          handleSubmit={handleSubmit}
+          handleSetEmail={handleSetEmail}
+          handleSetPassword={handleSetPassword}
+        />
+      </Box>
     </>
   )
 }
